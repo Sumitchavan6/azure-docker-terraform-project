8d2828c318211f18713e1243010fd861f2cf7ff0
Sumitchavan6 <162594338+Sumitchavan6@users.noreply.github.com>
2026-05-24 13:12:01 +0530

    Clear subscription and tenant IDs in provider.tf
    Removed sensitive subscription and tenant IDs from provider configuration.

--- infra/provider.tf
+++ infra/provider.tf
@@ -11,7 +11,7 @@ terraform {
 }
 provider "azurerm" {
   features {}
-subscription_id ="817fbfea-a661-43fb-a5f5-654bf0b2475e"
-tenant_id = "6557bae0-e0d1-4268-8782-39d8cadbefe7"
+subscription_id =""
+tenant_id = ""
 }
 
